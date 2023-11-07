00b31f09d9822554913de1ed24c9ef18406271ad
narthkings <imohdumto@gmail.com>
2023-11-07 11:48:05 -0500

    Initial Commit

---
+++ War Card Game/ContentView.swift
@@ -0,0 +1,82 @@
+//
+//  ContentView.swift
+//  War Card Game
+//
+//  Created by jaston on 10/30/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+   @State var playerCard = "card5"
+   @State var cpuCard = "card3"
+   @State var playerScore: Int = 0
+   @State var cpuScore: Int = 0
+    var body: some View {
+        
+        ZStack{
+            Image("background-plain")
+                .resizable()
+                .ignoresSafeArea()
+            
+            VStack(
+                spacing: 50
+            ){
+                Image("logo")
+                
+                HStack{
+                    Spacer()
+                    Image(playerCard)
+                    Spacer()
+                    Image(cpuCard)
+                    Spacer()
+                }
+                
+                Button(action: {
+                    deal()
+                }, label: {
+                    Image("button")
+                })
+
+                HStack{
+                    Spacer()
+                    VStack{
+                        Text("Player").padding(.bottom, 10)
+                        Text(String(playerScore)).font(.largeTitle)
+                    }
+                    Spacer()
+                    VStack{
+                        Text("CPU").padding(.bottom, 10)
+                        Text(String(cpuScore)).font(.largeTitle)
+                    }
+                    Spacer()
+                }
+                .font(.headline)
+                .foregroundStyle(.white).fontWeight(.semibold)
+            }
+        }
+    }
+    
+    func deal(){
+       // randomize the player number
+        playerCard = "card" + String(Int.random(in: 2...14))
+        
+        // randomize the CPU's number
+         cpuCard  = "card" + String(Int.random(in: 2...14))
+        
+        // update the score sheet
+        
+        if playerCard > cpuCard{
+            playerScore+=1
+        }else if playerCard < cpuCard{
+            cpuScore+=1
+        }else {
+            return
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by jaston on 10/30/23.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
